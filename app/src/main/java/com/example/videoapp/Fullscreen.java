4af924aeb3f08d4f6dc2db5ab75c405c7b504fc4
ajay singh <singhajay9009735865@gmail.com>
2020-11-09 00:51:25 +0530

    new commit

--- app/src/main/java/com/example/videoapp/Fullscreen.java
+++ app/src/main/java/com/example/videoapp/Fullscreen.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -41,16 +42,16 @@ public class Fullscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Fullscreen");
+       // ActionBar actionBar = getSupportActionBar();
+       // actionBar.setTitle("Fullscreen");
 
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+       // actionBar.setDisplayHomeAsUpEnabled(true);
+       // actionBar.setDisplayShowHomeEnabled(true);
 
         playerView = findViewById(R.id.exoplayer_fullscreen);
         textView = findViewById(R.id.tv_fullscreen);
 
-        fullscreenButton = playerView.findViewById(R.id.fullscreen_icon);
+        fullscreenButton = playerView.findViewById(R.id.exoplayer_fullscreen_icon);
 
 
         Intent intent = getIntent();
@@ -63,53 +64,54 @@ public class Fullscreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(fullscreen){
-                    fullscreenButton.setImageDrawable(ContextCompat.getDrawable(Fullscreen.this,R.drawable.ic_fullscreen_expand));
+                if (fullscreen) {
+                    fullscreenButton.setImageDrawable(ContextCompat.getDrawable(Fullscreen.this, R.drawable.ic_fullscreen_expand));
                     getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-                    if (getSupportActionBar() != null){
+                    if (getSupportActionBar() != null) {
                         getSupportActionBar().show();
                     }
 
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)playerView.getLayoutParams();
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
                     params.width = params.MATCH_PARENT;
                     params.height = (int) (200 * getApplicationContext().getResources().getDisplayMetrics().density);
                     playerView.setLayoutParams(params);
                     fullscreen = false;
-                }else
-                    {
-                        fullscreenButton.setImageDrawable(ContextCompat.getDrawable(Fullscreen.this,R.drawable.ic_fullscreen_skrink));
-                        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-                        if (getSupportActionBar() != null){
-                            getSupportActionBar().hide();
-                        }
-                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)playerView.getLayoutParams();
-                        params.width = params.MATCH_PARENT;
-                        params.height = params.MATCH_PARENT;
-                        playerView.setLayoutParams(params);
-                        fullscreen = true;
+                    textView.setVisibility(view.VISIBLE);
+                } else {
+                    fullscreenButton.setImageDrawable(ContextCompat.getDrawable(Fullscreen.this, R.drawable.ic_fullscreen_skrink));
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                    if (getSupportActionBar() != null) {
+                        getSupportActionBar().hide();
                     }
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
+                    params.width = params.MATCH_PARENT;
+                    params.height = params.MATCH_PARENT;
+                    playerView.setLayoutParams(params);
+                    fullscreen = true;
+                    textView.setVisibility(view.INVISIBLE);
+                }
             }
         });
     }
 
-    private MediaSource buildMediaSource(Uri uri){
+    private MediaSource buildMediaSource(Uri uri) {
         DataSource.Factory datasourcefactory =
                 new DefaultHttpDataSourceFactory("video");
         return new ProgressiveMediaSource.Factory(datasourcefactory)
                 .createMediaSource(uri);
     }
 
-    private void initializeplayer(){
+    private void initializeplayer() {
         player = ExoPlayerFactory.newSimpleInstance(this);
         playerView.setPlayer(player);
         Uri uri = Uri.parse(url);
         MediaSource mediaSource = buildMediaSource(uri);
         player.setPlayWhenReady(playwhenready);
-        player.seekTo(currentWindow,playbackposition);
-        player.prepare(mediaSource,false,false);
+        player.seekTo(currentWindow, playbackposition);
+        player.prepare(mediaSource, false, false);
 
     }
 
@@ -117,7 +119,7 @@ public class Fullscreen extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        if (Util.SDK_INT >= 26){
+        if (Util.SDK_INT >= 26) {
             initializeplayer();
         }
     }
@@ -126,7 +128,7 @@ public class Fullscreen extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        if (Util.SDK_INT >= 26 || player == null){
+        if (Util.SDK_INT >= 26 || player == null) {
             //initializeplayer();
         }
     }
@@ -135,7 +137,7 @@ public class Fullscreen extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        if (Util.SDK_INT > 26){
+        if (Util.SDK_INT > 26) {
             releasePlayer();
         }
     }
@@ -144,16 +146,29 @@ public class Fullscreen extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        if (Util.SDK_INT >= 26){
+        if (Util.SDK_INT >= 26) {
             releasePlayer();
         }
     }
 
-    private void releasePlayer(){
-        if (player != null)
+    private void releasePlayer() {
+        if (player != null) {
             playwhenready = player.getPlayWhenReady();
             playbackposition = player.getCurrentPosition();
             currentWindow = player.getCurrentWindowIndex();
             player = null;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        player.stop();
+        releasePlayer();
+
+        final Intent intent = new Intent();
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
